04e2bd7b1718b48937e38b34a035aacb1321dc83
Sampreet <sam.learnnew@gmail.com>
2020-10-17 20:53:21 -0700

    Added and tested the route functionalities for controllers/authorRoutes.js

--- controllers/authorRoutes.js
+++ controllers/authorRoutes.js
@@ -1,26 +1,93 @@
 const express = require('express');
+const { findById } = require('../models/Author');
 const router = express.Router();
 
 const Author = require('../models/Author');
 const Cookbook = require('../models/Cookbook');
 
 // Write the route to list all authors
-// GET ROUTE - INDEX
-const index = async (req, res) => {
+// GET ROUTE - ACTION INDEX
+router.get('/', async (req, res) => {
 	try {
-		const data = await Author.find({});
+		const data = await Author.find({}).populate('cookbooks');
 		res.json({ status: 200, data: data });
 	} catch (err) {
 		res.json({ status: 200, error: err.message });
 	}
-};
-router.get('/', (req, res) => index(req, res));
+});
 
 // Write the route to get authors by firstname
+// GET ROUTE - ACTION SHOW
+router.get('/firstName/:firstName', async (req, res) => {
+	try {
+		const data = await Author.find({
+			firstName: { $regex: req.params.firstName, $options: 'i' },
+		}).populate('cookbooks');
+		res.json({ status: 200, data: data });
+	} catch (err) {
+		res.json({ status: 200, error: err.message });
+	}
+});
 
 // Write the route to create an author:
+// POST ROUTE - ACTION CREATE
+router.post('/', async (req, res) => {
+	try {
+		await Author.create(req.body);
+		const data = await Author.find({}).populate('cookbooks');
+		res.json({ status: 200, data: data });
+	} catch (err) {
+		res.json({ status: 200, error: err.message });
+	}
+});
 
 // Write the route to update an author
+// PUT ROUTE - ACTION UPDATE
+router.put('/id/:id', async (req, res) => {
+	try {
+		await Author.findByIdAndUpdate(req.params.id, req.body, { new: true });
+		const data = await Author.find({}).populate('cookbooks');
+		res.json({ status: 200, data: data });
+	} catch (err) {
+		res.json({ status: 200, error: err.message });
+	}
+});
+
+// Adding Cookbook id to Author
+// localhost:4000/api/authors/id/5f8bb507fd78c51394523b42/addCookbook/5f8bb6420be09615d3340b2a
+//localhost:4000/api/authors/id/5f8bb507fd78c51394523b42/addCookbook/5f8bb6490be09615d3340b2b
+router.put('/id/:id/addCookbook/:cookbookId', async (req, res) => {
+	try {
+		const cookbook = await Cookbook.findById(req.params.cookbookId);
+		await Author.findByIdAndUpdate(
+			req.params.id,
+			{ $addToSet: { cookbooks: cookbook.id } },
+			{ new: true }
+		);
+		const data = await Author.find({}).populate('cookbooks');
+		res.json({ status: 200, data: data });
+	} catch (err) {
+		res.json({ status: 200, error: err.message });
+	}
+});
+
+// removing Cookbook id from Author
+// Sample URL - localhost:4000/api/authors/id/5f8bb507fd78c51394523b42/removeCookbook/5f8bb6420be09615d3340b2a
+//localhost:4000/api/authors/id/5f8bb507fd78c51394523b42/removeCookbook/5f8bb6490be09615d3340b2b
+router.put('/id/:id/removeCookbook/:cookbookId', async (req, res) => {
+	try {
+		const cookbook = await Cookbook.findById(req.params.cookbookId);
+		await Author.findByIdAndUpdate(
+			req.params.id,
+			{ $pull: { cookbooks: cookbook.id } },
+			{ new: true }
+		);
+		const data = await Author.find({}).populate('cookbooks');
+		res.json({ status: 200, data: data });
+	} catch (err) {
+		res.json({ status: 200, error: err.message });
+	}
+});
 
 // Update the cookbook using Postman.
 
